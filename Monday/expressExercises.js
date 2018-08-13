3d84c92c057c5a5f20738b670951c6786cb9b04c
Namaslay33 <skylar.hunt@utexas.edu>
2018-08-13 13:41:46 -0500

    First Route Parameter

--- Monday/expressExercises.js
+++ Monday/expressExercises.js
@@ -9,7 +9,14 @@ app.use(require('./Routes/cats'));
 app.use(require('./Routes/dogs'));
 app.use(require('./Routes/cats_and_dogs'));
 
+app.get('/greet/:name', function(request, response){
+    var name = request.params.name;
+
+    response.send("Hello " + name);
+});
+
 
 app.listen(8000, function(){
     console.log('Listening on port 8000');
 });
+
